135cd975e4929c2627b1071fdefee91419cdfbcd
Lucídio Souza <lucidio.abs@gmail.com>
2019-06-04 10:24:21 -0400

    add attackRegionComponent

---
+++ TrainGame_Two/Components/AttackRegionComponent.swift
@@ -0,0 +1,12 @@
+//
+//  AttackRegionComponent.swift
+//  TrainGame_Two
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 04/06/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+class AttackRegionComonent: GKComponent { }
