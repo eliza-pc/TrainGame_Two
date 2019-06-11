beb910d1945f6affd4e7b179c0a9e5b6ab4788a3
Lucídio Souza <lucidio.abs@gmail.com>
2019-06-11 12:01:41 -0400

    caixa puzzle, update jump

--- TrainGame_Two/Components/PhysicComponent.swift
+++ TrainGame_Two/Components/PhysicComponent.swift
@@ -17,6 +17,7 @@ class PhysicComponent: GKComponent {
     var nodePhysic: SKSpriteNode
     
     init(gameScene: GameScene, nodeName: String) {
+        print(gameScene.childNode(withName: nodeName) as? SKSpriteNode)
         nodePhysic = (gameScene.childNode(withName: nodeName) as? SKSpriteNode)!
         super.init()
     }
