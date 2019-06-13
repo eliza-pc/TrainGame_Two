a1c9c7485ae7d8477e9fe1b885dd9d16c2f92280
Robson James <james.robson.junior@gmail.com>
2019-06-13 11:02:59 -0400

    add assets e ToBeContinueController

---
+++ TrainGame_Two/Controller/ToBeContinueViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PauseViewController.swift
+//  TrainGame_Two
+//
+//  Created by Eliza Maria Porto de Carvalho, Robson James Junior, Lucídio Andrade Barbosa de Souza e André Afonso @Raj on 2019.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+// #part of the credits to Vilar da Camara Neto
+
+import UIKit
+
+
+class ToBeControllerViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
