2392355dbf130150106f53c4a1dfce9993a80aad
Robson James <james.robson.junior@gmail.com>
2019-06-13 19:25:58 -0400

    Bug do Balão

--- TrainGame_Two/Components/BalloonComponent.swift
+++ TrainGame_Two/Components/BalloonComponent.swift
@@ -21,8 +21,20 @@ class BalloonComponent: GKComponent {
     init(parentNode: SKNode, balloonNodeName: String) {
         self.balloon = parentNode.childNode(withName: balloonNodeName) as! SKSpriteNode
         
-        self.balloon.size = arrayEnigmas[0].size()
-        self.balloon.position = CGPoint(x: -20, y: 60)
+        switch parentNode.name {
+        case "NodeSoundBox-1":
+            self.balloon.size = CGSize(width: 588, height: 213)
+            self.balloon.position = CGPoint(x: -30, y: 20)
+            self.balloon.texture = arrayEnigmas[1]
+        case "NodeSoundBox-2":
+//            self.balloon.position = CGPoint(x: -220, y: 20)
+            self.balloon.position = CGPoint(x: -20, y: 60)
+            self.balloon.size = CGSize(width: 588, height: 213)
+            self.balloon.texture = arrayEnigmas[2]
+        default:
+            print("erro, don't identify Node")
+        }
+    
         
     //    self.balloon.texture = arrayEnigmas[0]
         self.balloon.alpha = 0
@@ -43,12 +55,11 @@ class BalloonComponent: GKComponent {
         case 1:
         
             self.balloon.size = CGSize(width: 588, height: 213)
-            self.balloon.position = CGPoint(x: -220, y: 20)
+            self.balloon.position = CGPoint(x: -30, y: 20)
             self.balloon.texture = arrayEnigmas[index]
          
         case 2:
             
-            self.balloon.position = CGPoint(x: -220, y: 20)
             self.balloon.size = CGSize(width: 588, height: 213)
             self.balloon.texture = arrayEnigmas[index]
             
